5539c58b60032685997df7a15c47aa05f70652b4
Foyceek <148572907+Foyceek@users.noreply.github.com>
2024-11-30 16:50:02 +0100

    Add files via upload

---
+++ ESP-IDF/radiotap_csv_pcap_2/main/variables.h
@@ -0,0 +1,26 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+#include <stdint.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define TOP_REQUESTS_COUNT 10
+
+extern int request_index;
+extern int max_request_rank;
+
+extern bool oled_initialized;
+extern bool off_extended_delay;
+
+typedef struct {
+    int rssi;
+    char mac_address[18];
+    time_t timestamp;
+} top_request_t;
+
+extern top_request_t top_requests[TOP_REQUESTS_COUNT];
+
+extern int rssi_ranges[10];
+
+#endif
